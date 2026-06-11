2af86e4644c093332557ef49b643660c9d331b0a
MishaPashkov <misapaskov23@gmail.com>
2026-06-12 00:04:05 +0300

    Fix deploy files

--- library_system/settings.py
+++ library_system/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-da5x4dy8-!g-zdr$qk%mw8!wu3-@ruh&64$^ay0s1)!mthpcmh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "digital-library-akib.onrender.com",
+    ".onrender.com",
+    "localhost",
+    "127.0.0.1"]
 
 
 # Application definition
@@ -44,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,8 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [BASE_DIR / 'static']
+#STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 MEDIA_URL = '/media/'
